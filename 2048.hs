8814f692fa9d2e2e5257473bcb800d5ce8b24385
Evan Rutledge Borden <evan@evan-borden.com>
2014-04-01 18:12:59 -0400

    game loop, random position generation, key interactions, todo.

--- 2048.hs
+++ 2048.hs
@@ -1,18 +1,21 @@
 module TwentyFortyEight () where
 
-import Data.List (transpose)
+import Data.List (transpose, elemIndices)
+import System.Console.ANSI
+import System.IO
+import Control.Applicative
+import System.Random (randomRIO)
 
-type Tile = Integer
+type Tile = Int
 type Row = [Tile]
 type Board = [Row]
-type BoardPosition = (Integer, Integer)
+type BoardPosition = (Int, Int)
 
-board :: Board
-board = [ [0, 0, 0, 0]
-        , [0, 0, 0, 0]
-        , [0, 0, 0, 0]
-        , [0, 0, 0, 0]
-        ]
+buildBoard :: Int -> Int -> Board
+buildBoard x y = replicate y (replicate x 0)
+
+startBoard :: BoardPosition -> Board -> Board
+startBoard x b = mutateBoard b x 2 
 
 -- Change a tile on the board
 mutateBoard :: Board -> BoardPosition -> Tile -> Board
@@ -21,12 +24,26 @@ mutateBoard (x:xs) (r, c) t
     | otherwise = mutateRow x c t:xs
 
 -- Change a tile in a row
-mutateRow :: Row -> Integer -> Tile -> Row
+mutateRow :: Row -> Int -> Tile -> Row
 mutateRow (x:xs) p t
     | p > 0     = x:mutateRow xs (p - 1) t
     | otherwise = t:xs
 
+-- Get empty cell positions
+emptyCells b = concat $ foldl (\a r -> a ++ [zip (repeat $ length a) (emptyRowCells r)]) [] b
+
+emptyRowCells :: Row -> [Int]
+emptyRowCells r = elemIndices 0 r
+
 -- Key movements
+keyPress :: Board -> Char -> Board
+keyPress b x = case x of
+    'a' -> shiftLeft b
+    'w' -> shiftUp b
+    'd' -> shiftRight b
+    's' -> shiftDown b
+    _   -> b
+
 shiftLeft :: Board -> Board
 shiftLeft b = map (sumRowLeft . shiftRowLeft) $ b
 
@@ -56,28 +73,35 @@ sumRowLeft (x:y:xs)
               else x:(sumRowLeft (y:xs))
     | x == 0 = (sumRowLeft (y:xs)) ++ [0]
 
+
+{------------------------------|
+       Impure IO Business
+-------------------------------}
+
+-- Get a random list element
+pickRand :: [a] -> IO a
+pickRand xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
+
 -- Print the board in a semi friendly manner
 printBoard b = mapM (putStrLn) $ map show b
 
+gameLoop :: Board -> IO Board
+gameLoop b = do
+    -- Add random tile
+    rando <- pickRand $ emptyCells b
+    let b2 = mutateBoard b rando 2
+
+    -- Display the world
+    clearScreen
+    putStrLn ""
+    printBoard b2
+
+    -- Do key handling
+    return =<< gameLoop =<< keyPress b2 <$> getChar
+
 main = do
-    let board2 = mutateBoard board (1, 1) 2
-    let board3 = mutateBoard board2 (1, 3) 2
-    let board4 = mutateBoard board3 (1, 2) 2
-    printBoard board
-    putStrLn ""
-    printBoard board2
-    putStrLn ""
-    printBoard $ shiftLeft board2
-    putStrLn ""
-    printBoard board3
-    putStrLn ""
-    printBoard $ shiftLeft board3
-    putStrLn ""
-    printBoard $ shiftRight board3
-    putStrLn ""
-    printBoard $ shiftUp board3
-    putStrLn ""
-    printBoard $ shiftDown board3
-    putStrLn ""
-    printBoard $ shiftLeft board4
-    putStrLn ""
+    hSetBuffering stdin NoBuffering
+
+    gameLoop $ startBoard (1, 2) (buildBoard 4 4)
+    
+    return ()
