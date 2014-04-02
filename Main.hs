12cd125090c5ce3d526bad02466b7e51731a816d
Evan Rutledge Borden <evan@evan-borden.com>
2014-04-02 00:39:31 -0400

    seperated game logic from display logic

---
+++ Main.hs
@@ -0,0 +1,64 @@
+module Main (main) where
+
+import TwentyFortyEight
+import System.Console.ANSI
+import System.IO
+
+-- Combine two strings and overwrite
+overwrite :: [a] -> [a] -> [a]
+overwrite [] _ = []
+overwrite [x] _ = [x]
+overwrite (x:xs) [] = x:overwrite xs []
+overwrite (x:xs) [y] = y:overwrite xs []
+overwrite (x:xs) (y:ys) = y:overwrite xs ys
+
+-- Print the board in a semi friendly manner
+printBoard = mapM (\x -> do
+    printRow x
+    putStr "\n")
+
+printRow r = mapM (printCell) r
+
+printCell c = colorByInt c $ overwrite "    " (show c :: String)
+
+colorByInt x
+    | x == 2 = colorStr Vivid Black Vivid White
+    | x == 4 = colorStr Vivid Black Dull White
+    | x == 8 = colorStr Vivid White Vivid Magenta
+    | x == 16 = colorStr Vivid White Dull Magenta
+    | x == 32 = colorStr Vivid White Vivid Blue
+    | x == 64 = colorStr Vivid White Dull Blue
+    | x == 128 = colorStr Vivid White Vivid Red
+    | x == 256 = colorStr Vivid Black Vivid Yellow
+    | x == 512 = colorStr Vivid Black Dull Yellow
+    | x == 1024 = colorStr Vivid White Vivid Green
+    | x == 2048 = colorStr Vivid White Vivid Cyan
+    | otherwise = putStr
+
+colorStr :: ColorIntensity -> Color -> ColorIntensity -> Color -> String -> IO ()
+colorStr fgi fg bgi bg str = do
+  setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
+  putStr str
+  setSGR []
+
+-- Where the magic happens
+main = do
+    -- Turn input buffering off so key presses don't need an enter
+    hSetBuffering stdin NoBuffering
+
+    hideCursor
+
+    -- Kick off the game loop with a fresh history
+    final <- gameLoop [(startBoard (2, 2) (buildBoard 4 4), 0)] (\(board, score) -> do
+        -- Display the world
+        clearScreen
+        setCursorPosition 0 0
+        putStrLn "2048.hs"
+        putStrLn ""
+        putStrLn $ "Score: " ++ (show $ sumBoard board)
+        putStrLn ""
+        printBoard board
+        )
+
+    mapM (putStrLn) ["", "", "Game Over"]
+    return ()
--- TwentyFortyEight.hs
+++ TwentyFortyEight.hs
@@ -1,8 +1,11 @@
-module TwentyFortyEight () where
+module TwentyFortyEight
+    ( gameLoop
+    , sumBoard
+    , buildBoard
+    , startBoard
+    ) where
 
 import Data.List (transpose, elemIndices, intersperse)
-import System.Console.ANSI
-import System.IO
 import Control.Applicative
 import System.Random (randomRIO)
 
@@ -88,13 +91,6 @@ sumRowLeft (x:y:xs)
 sumBoard :: Board -> Int
 sumBoard x = sum (map sum x)
 
--- Combine two strings and overwrite
-overwrite :: [a] -> [a] -> [a]
-overwrite [] _ = []
-overwrite [x] _ = [x]
-overwrite (x:xs) [] = x:overwrite xs []
-overwrite (x:xs) [y] = y:overwrite xs []
-overwrite (x:xs) (y:ys) = y:overwrite xs ys
 
 {------------------------------|
        IO Business
@@ -135,53 +131,3 @@ gameLoop h func = do
     
     -- Do key handling
     return =<< gameLoop ((newBoard, 0):(currentBoard, 0):(tail h)) func
-
--- Print the board in a semi friendly manner
-printBoard = mapM (\x -> do
-    printRow x
-    putStr "\n")
-
-printRow r = mapM (printCell) r
-
-printCell c = colorByInt c $ overwrite "    " (show c :: String)
-
-colorByInt x
-    | x == 2 = colorStr Vivid Black Vivid White
-    | x == 4 = colorStr Vivid Black Dull White
-    | x == 8 = colorStr Vivid White Vivid Magenta
-    | x == 16 = colorStr Vivid White Dull Magenta
-    | x == 32 = colorStr Vivid White Vivid Blue
-    | x == 64 = colorStr Vivid White Dull Blue
-    | x == 128 = colorStr Vivid White Vivid Red
-    | x == 256 = colorStr Vivid Black Vivid Yellow
-    | x == 512 = colorStr Vivid Black Dull Yellow
-    | x == 1024 = colorStr Vivid White Vivid Green
-    | x == 2048 = colorStr Vivid White Vivid Cyan
-    | otherwise = putStr
-
-colorStr :: ColorIntensity -> Color -> ColorIntensity -> Color -> String -> IO ()
-colorStr fgi fg bgi bg str = do
-  setSGR [SetColor Foreground fgi fg, SetColor Background bgi bg]
-  putStr str
-  setSGR []
-
--- Where the magic happens
-main = do
-    -- Turn input buffering off so key presses don't need an enter
-    hSetBuffering stdin NoBuffering
-
-    hideCursor
-
-    -- Kick off the game loop with a fresh history
-    gameLoop [(startBoard (2, 2) (buildBoard 4 4), 0)] (\(board, score) -> do
-        -- Display the world
-        clearScreen
-        setCursorPosition 0 0
-        putStrLn "2048.hs"
-        putStrLn ""
-        putStrLn $ "Score: " ++ (show $ sumBoard board)
-        putStrLn ""
-        printBoard board
-        )
-    
-    return ()
